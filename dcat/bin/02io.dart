8e3769ad0db695ef397b7565291005315e4930cc
Singawe Jack <chenming@mail.ynu.edu.cn>
2023-05-22 02:24:50 +0000

    add

---
+++ dcat/bin/02io.dart
@@ -0,0 +1,9 @@
+// import 'dart:io';
+
+// void main() {
+//   var showLineNumbers,lineNumber,line;
+//   if (showLineNumbers) {
+//     stdout.write('${lineNumber++} ');
+//   }
+//   stdout.writeln(line);
+// }
